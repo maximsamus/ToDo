3588b9fd6a585f5d9b50fa6df01ec3916974132e
maximsamus <maximsamus1@gmail.com>
2022-08-27 12:41:55 +0300

    CoreData rename

--- ToDo/Model/Task.swift
+++ ToDo/Model/Task.swift
@@ -5,7 +5,7 @@
 //  Created by Максим Самусь on 24.08.2022.
 //
 
-struct Task: Codable {
-    let title: String
-    var done: Bool
-}
+//struct Task: Codable {
+//    let title: String
+//    var done: Bool
+//}
