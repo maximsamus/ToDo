1351855f8a1fbdcf5940b852bbe6daa93c7ba507
maximsamus <maximsamus1@gmail.com>
2022-08-28 18:11:47 +0300

    create Database

---
+++ ToDo/Model/Data.swift
@@ -0,0 +1,13 @@
+//
+//  Data.swift
+//  ToDo
+//
+//  Created by Максим Самусь on 28.08.2022.
+//
+//import Foundation
+import RealmSwift
+
+class Data: Object {
+    @Persisted var name = ""
+    
+}
